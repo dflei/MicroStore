261f93638fc83023bf5875c1563a9cb1963a2ada
Jiajin Lei <jlei@abpeds.org>
2018-11-08 11:55:27 -0500

    more modifiction

--- Hello.cpp
+++ Hello.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello world! I like geninks which is a great tool for continuos intigration and deployment" << endl;
+        cout << "Hope have a wonderful lift"<<endl;
     return 0;
 }
