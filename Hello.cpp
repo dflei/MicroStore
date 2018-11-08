3fa0a7d0755b4c091c2d80123e24eaa76db68ae6
Jiajin Lei <jlei@abpeds.org>
2018-11-08 11:25:32 -0500

    more modification

--- Hello.cpp
+++ Hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world! I like geninks" << endl;
+	cout << "Hello world! I like geninks which is a great tool for continuos intigration and deployment" << endl;
     return 0;
 }
