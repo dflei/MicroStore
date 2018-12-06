01f6f8db52c8f1fa067f3712701a1ebeedccd5dc
dflei <l0826.2015@yahoo.coM>
2018-12-06 13:01:50 -0500

    message

--- Hello.cpp
+++ Hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world! I like geninks which is a great tool for continuos intigration and deployment" << endl;
-        cout << "Hope have a wonderful lift"<<endl;
+	cout << "Hello world! Hello Jenkis!" << endl;
+        cout << "Hope have a wonderful life"<<endl;
     return 0;
 }
