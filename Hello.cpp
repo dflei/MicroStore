8c89522dccfc4838d56bbc2ead2080e86edbf6e6
dflei <l0826.2015@yahoo.coM>
2018-12-06 13:13:47 -0500

    MESSAGE

--- Hello.cpp
+++ Hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world! Hello Jenkis!" << endl;
+	cout << "Hello world! Hello Jenkis! Hello SCRIP!" << endl;
         cout << "Hope have a wonderful life"<<endl;
     return 0;
 }
